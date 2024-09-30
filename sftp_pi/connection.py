c1b43bd5bc325179bab4f397f88b859d1704bdd1
Vierce1 <a.westermann.19@gmail.com>
2024-09-30 14:02:28 -0500

    directory exclusion update

--- sftp_pi/connection.py
+++ sftp_pi/connection.py
@@ -25,34 +25,37 @@ class Connection (dict):
 
     def connect(self) -> pysftp.Connection:
         return pysftp.Connection(self['host'], username=self['user'],
-                            private_key=self['private key path'], port=self['port'], cnopts=self['cnopts'])
+                                 private_key=self['private key path'], port=self['port'], cnopts=self['cnopts'])
 
     def test_connection(self):
         conn = self.connect()
         print(conn.execute('ls'))
 
     def upload_file(self, local_file_path, remote_path):
-        self.connection.put(local_file_path, remote_path)
+        self.connection.put(f'{local_file_path}', f'{remote_path}' ,confirm=False)
 
     def upload_dir(self, local_dir, remote_dir, dir_exclusions: [str], file_suffix_exclusions: [str]):
         for entry in os.listdir(local_dir):
             if local_dir.split('\\')[-1] in dir_exclusions:
-                return
+                continue
             remote_path = remote_dir + '/' + entry
             local_path = os.path.join(local_dir, entry)
-            print(local_path)
-            print(remote_path)
+            print(f'Uploading.\tlocal: {local_path}\t\tremote: {remote_path}')
             if not os.path.isfile(local_path):
                 try:
-                    self.connection.mkdir(remote_path)
-                except OSError:
-                    pass
+                    if local_path.split('\\')[-1] in dir_exclusions:
+                        continue
+                    else:
+                        self.connection.mkdir(remote_path)
+                except OSError as e:
+                    print(f'error making dir  {remote_path}:   {e}')
+
                 self.upload_dir(local_path, remote_path, dir_exclusions, file_suffix_exclusions)
             elif len([x for x in file_suffix_exclusions if local_path.endswith(x)]) == 0:
                 self.upload_file(local_path, remote_path)
 
-    def download(self, remote_path, local_path):
-        self.connection.get(remote_path, local_path)
+def download(self, remote_path, local_path):
+    self.connection.get(remote_path, local_path)
 
 
 # c = Connection('../config.json', '../hostkey.ppk', '../private_key.ppk')
